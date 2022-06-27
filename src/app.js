c26983471c5bd7b884bb3852e680a21c4bda328e
burn-Thor <eothorburn@gmail.com>
2022-06-27 09:57:49 +0100

    completed project

--- src/app.js
+++ src/app.js
@@ -1,15 +1,36 @@
 require("./db/connection")
 const mongoose = require("mongoose");
 const yargs = require("yargs");
-const {addMovie, listMovies} = require("./movies/movieMethods");
+const {addMovie, listMovies, deleteMovie, updateMovie} = require("./movies/movieMethods");
 
 const app = async (yargsObj) => {
     try {
+    // add Films
         if (yargsObj.add) {
-            await addMovie({ title: yargsObj.title, actor: yargsObj.actor})
+            await addMovie({ 
+                title: yargsObj.title, 
+                actor: yargsObj.actor,
+                year: yargs.Obj.year})
             console.log(await listMovies())
+    // list Films
         } else if (yargsObj.list){
             console.log(await listMovies());
+    // Delete FIlms
+        } else if (yargsObj.delete) {
+            await removeMovie({
+                title: yargsObj.title,
+                director: yargsObj.director,
+                year: theEdits.year,
+            });
+            console.log(await listMovies());
+    // Edit FIlms
+         } else if (yargsObj.edit) {
+             await editMovie(yargsObj.edit, {
+                title: yargsObj.title,
+                actor: yargsObj.director,
+                year: yargsObj.year,
+    });
+    console.log(await listMovies());
         } else {
             console.log("Incorrect command")
         }
--- src/movies/movieMethods.js
+++ src/movies/movieMethods.js
@@ -18,8 +18,30 @@ exports.listMovies = async () => {
 };
 
 // create an update one or update many function
+exports.updateMovie = async (theOriginal, theUpdate) => {
+    try {
+        const movieTitle = {title: theOriginal };
+        const newData = {
+            $set: {
+                title: theUpdate.title,
+                director: theUpdate.director,
+                year: theUpdate.year,
+            },
+        };
+        const updateMovie = await Movie.updateOne(movieTitle, newData);
+        console.log(editEntry);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 
 
 // create a delete one or delete many function
-
+exports.deleteMovie = async (movieObj) => {
+    try {
+      await Movie.deleteOne(movieObj);
+    } catch (error) {
+      console.log(error);
+    }
+  };
